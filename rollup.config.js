a5d6bc79931f103c7c5e7498b836aa946373822b
myTerminal <team.terminal@gmail.com>
2019-10-31 20:10:52 +0000

    update rollup configuration, fix broken build

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,30 @@
+import external from 'rollup-plugin-peer-deps-external';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-import babelrc from 'babelrc-rollup';
-import less from 'rollup-plugin-less';
+import { uglify } from 'rollup-plugin-uglify';
+import postcss from 'rollup-plugin-postcss';
+
+import packageDetails from './package.json';
 
 export default {
     input: 'src/scripts/template-web-library.js',
     output: {
-        file: 'build/scripts/template-web-library.js',
+        file: packageDetails.main,
         format: 'umd',
         name: 'templateWebLibrary'
     },
     plugins: [
-        babel(babelrc()),
-        less()
+        external(),
+        resolve(),
+        commonjs(),
+        babel(),
+        process.env.NODE_ENV === 'production' && uglify(),
+        postcss({
+            extract: true,
+            extensions: [
+                '.less'
+            ]
+        })
     ]
 }
